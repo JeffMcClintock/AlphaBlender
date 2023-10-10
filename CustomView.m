26aa3f818d58ecf146c6a8e74de9a880d91cad64
Jeff McClintock <jef@synthedit.com>
2023-10-11 12:14:43 +1300

    Update CustomView.m

--- CustomView.m
+++ CustomView.m
@@ -17,10 +17,11 @@
     [NSBezierPath fillRect:dirtyRect];
     
     [[NSColor whiteColor] set];
-    [NSBezierPath fillRect:NSMakeRect(0, 20, 300, 60)];
+    [NSBezierPath fillRect:NSMakeRect(-60, 20, 360, 60)];
     
     [[NSColor blackColor] set];
-    [NSBezierPath fillRect:NSMakeRect(60, 20, 60, 60)];
+    [NSBezierPath fillRect:NSMakeRect(0, 20, 60, 60)];
+
     [NSBezierPath fillRect:NSMakeRect(180, 20, 60, 60)];
 
     // checkerboard
@@ -31,14 +32,18 @@
         {
             if(fill)
             {
-                [NSBezierPath fillRect:NSMakeRect(120 + x, 20 + y, 1, 1)];
+                [NSBezierPath fillRect:NSMakeRect(60 + x, 20 + y, 1, 1)];
             }
             
             fill = !fill;
         }
         fill = !fill;
     }
-    
+
+    // 50% gray
+    [[NSColor colorWithCalibratedRed:0.5 green:0.5 blue:0.5 alpha:1.0] set];
+    [NSBezierPath fillRect:NSMakeRect(120, 20, 60, 60)];
+
     // 50% white over 100% black
     [[NSColor colorWithCalibratedRed:1.0 green:1.0 blue:1.0 alpha:0.5] set];
     [NSBezierPath fillRect:NSMakeRect(180, 20, 60, 60)];
@@ -46,7 +51,6 @@
     // 50% black over 100% white
     [[NSColor colorWithCalibratedRed:0.0 green:0.0 blue:0.0 alpha:0.5] set];
     [NSBezierPath fillRect:NSMakeRect(240, 20, 60, 60)];
-
 }
 
 @end
