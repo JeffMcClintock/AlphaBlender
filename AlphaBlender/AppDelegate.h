ae4d264bdd6a74409873a537a7cf7899ed551332
Jeff McClintock <jef@synthedit.com>
2023-10-11 11:16:17 +1300

    Initial Commit

---
+++ AlphaBlender/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  AlphaBlender
+//
+//  Created by Jeff McClintock on 11/10/23.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ AlphaBlender/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  AlphaBlender
+//
+//  Created by Jeff McClintock on 11/10/23.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
